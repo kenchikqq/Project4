120aef8d8fb1244dc5784fa1962ebd9e9d903aea
djser <djser@KENCHIK>
2024-09-19 13:49:12 +0500

    232

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,14 +1,52 @@
 #include <iostream>
+#include <windows.h>
 
 int main() {
-    std::string cowSays = "Hello World";
-    std::cout << "        (__)\n";
-    std::cout << "        (oo)\n";
-    std::cout << "  /-------\\/  \n";
-    std::cout << " / |     ||   \n";
-    std::cout << "*  /\---/\\    \n";
-    std::cout << "   ~~   ~~\n";
-    std::cout << "Сказала: \"" << cowSays << "\"\n";
+    setlocale(LC_ALL, "Russian");
+    
+    double firstNumber, secondNumber, result;
+    char operation;
+
+    
+    std::cout << "Простой калькулятор\n";
+    std::cout << "Введите первое число: ";
+    std::cin >> firstNumber;
+
+    std::cout << "Введите оператор (+, -, *, /, %): ";
+    std::cin >> operation;
+
+    std::cout << "Введите второе число: ";
+    std::cin >> secondNumber;
+
+    
+    if (operation == '+') {
+        result = firstNumber + secondNumber;
+        std::cout << "Результат: " << result << std::endl;
+    }
+    else if (operation == '-') {
+        result = firstNumber - secondNumber;
+        std::cout << "Результат: " << result << std::endl;
+    }
+    else if (operation == '*') {
+        result = firstNumber * secondNumber;
+        std::cout << "Результат: " << result << std::endl;
+    }
+    else if (operation == '/') {
+        if (secondNumber != 0) {
+            result = firstNumber / secondNumber;
+            std::cout << "Результат: " << result << std::endl;
+        }
+        else {
+            std::cout << "Ошибка: Деление на ноль!" << std::endl;
+        }
+    }
+    else if (operation == '%') {
+        result = (firstNumber * secondNumber) / 100;
+        std::cout << "Результат: " << result << std::endl;
+    }
+    else {
+        std::cout << "Ошибка: Неверный оператор!" << std::endl;
+    }
 
     return 0;
-} 
+}
