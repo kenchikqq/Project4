1f4eab18664c5cafd9b83589f4e754c4273a6d57
djser <djser@KENCHIK>
2024-09-19 12:58:48 +0500

    Добавьте файлы проекта.

---
+++ Project4/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::string cowSays = "Hello World";
+    std::cout << "        (__)\n";
+    std::cout << "        (oo)\n";
+    std::cout << "  /-------\\/  \n";
+    std::cout << " / |     ||   \n";
+    std::cout << "*  /\---/\\    \n";
+    std::cout << "   ~~   ~~\n";
+    std::cout << "Сказала: \"" << cowSays << "\"\n";
+
+    return 0;
+}
